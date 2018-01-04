3071215c416fa89bc56a62d421779ce4da4ec32a
Lindsay Lee <yasdnil922@comcast.net>
2018-01-04 14:05:10 -0500

    Create template for app, update .gitignore

--- app.R
+++ app.R
@@ -8,43 +8,104 @@
 #
 
 library(shiny)
+library(shinythemes)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
-)
+# Read in the pre-formatted data ----
 
-# Define server logic required to draw a histogram
+mdstest <- read.csv("data/mdstest.csv")
+
+# Load tidy table producing function ----
+
+
+# Load plotting function ----
+
+
+
+
+# Define UI ----
+ui <-
+  fluidPage(theme = shinytheme("yeti"),
+            
+            titlePanel("MDSViz"),
+            
+            sidebarLayout(
+              
+              sidebarPanel(
+                
+                h3("Options"),
+                
+                selectInput(
+                  "country",
+                  h5("Country"),
+                  choices = list("Chile" = "Chile",
+                                 "Sri Lanka" = "Sri Lanka"),
+                  selected = "Chile"
+                ),
+                
+                checkboxGroupInput(
+                  "indicators",
+                  h5("Indicators"),
+                  choices = list(
+                    "EF 1" = "B3001",
+                    "EF 2" = "B3002",
+                    "EF 3" = "B3003",
+                    "EF 4" = "B3004"
+                  ),
+                  selected = NULL
+                ),
+                
+                checkboxGroupInput(
+                  "disaggregators",
+                  h5("Disaggregators"),
+                  choices = list(
+                    "Sex" = "sex",
+                    "Age group" = "age_cat",
+                    "Disability level" = "performance_cat"
+                  ),
+                  selected = NULL
+                )
+              ),
+              
+              mainPanel(
+                textOutput("selected_country"),
+                textOutput("selected_indicators"),
+                textOutput("selected_disaggregators"),
+                plotOutput("graph"),
+                verbatimTextOutput("verb")
+              )
+            ))
+
+
+# Define server logic ----
 server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+  output$selected_country <- renderText({
+    paste("You have selected the country", input$country)
+  })
+  
+  output$selected_indicators <- renderText({
+    paste("You have selected the indicators", paste(input$indicators,collapse = ", "))
+  })
+  
+  output$selected_disaggregators <- renderText({
+    paste("You have selected the disaggregators", paste(input$disaggregators,collapse = ", "))
+  })
+  
+  output$graph <- renderPlot({
+
+    # tab <- tabFunc(...)
+    # plotFunc(tab,...)
+    
+    if (!is.null(input$indicators)) {
+      tab <- table(mdstest[, input$indicators])
+      plot(tab)
+    }
+  })
+  
+  # output$verb <- renderPrint({
+  #   input$indicators
+  #   head(mdstest[,input$indicators])
+  # })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
-
